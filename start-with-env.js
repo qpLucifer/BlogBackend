56f40f60183dd6a55392870fa27f080062d80915
moneyRoc <1799652305@qq.com>
2025-08-26 10:34:51 +0800

    fix: change cors

--- start-with-env.js
+++ start-with-env.js
@@ -11,7 +11,7 @@ process.env.DB_PASSWORD = '7jWW2waA74yZpGEx';
 process.env.PORT = 3000;
 process.env.JWT_SECRET = 'money_roc_secret_key';
 process.env.JWT_EXPIRES_IN = '1d';
-process.env.CORS_ORIGIN = 'https://www.jiayizhou.top:3002';
+process.env.CORS_ORIGIN = 'https://www.jiayizhou.top:3001';
 
 console.log('🚀 使用环境变量启动应用...');
 console.log('NODE_ENV:', process.env.NODE_ENV);
